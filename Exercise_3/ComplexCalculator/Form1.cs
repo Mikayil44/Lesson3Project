9ccb06412dcc3537c5d10db5ca4570010ba384a2
Mikayil44 <147695892+Mikayil44@users.noreply.github.com>
2025-09-30 22:33:54 +0400

    Form1.cs

--- Exercise_3/ComplexCalculator/Form1.cs
+++ Exercise_3/ComplexCalculator/Form1.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Windows.Forms;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -10,67 +11,101 @@ namespace ComplexCalculator
             InitializeComponent();
         }
 
+   
+
         private void CE_Click(object sender, EventArgs e)
         {
-            box.Clear();
+            box.Text = "0";
         }
 
         private void C_Click(object sender, EventArgs e)
         {
-            if (box.Text.Length > 0)
+            if (box.Text.Length > 1)
             {
                 box.Text = box.Text.Substring(0, box.Text.Length - 1);
             }
-        }
-
-        private void num1_Click(object sender, EventArgs e)
-        {
-            box.Text += "1";
-        }
-
-        private void num2_Click(object sender, EventArgs e)
-        {
-            box.Text += "2";
-        }
-
-        private void num3_Click(object sender, EventArgs e)
-        {
-            box.Text += "3";
-        }
-
-        private void num4_Click(object sender, EventArgs e)
-        {
-            box.Text += "4";
-        }
-
-        private void num5_Click(object sender, EventArgs e)
-        {
-            box.Text += "5";
-        }
-
-        private void num6_Click(object sender, EventArgs e)
-        {
-            box.Text += "6";
-        }
-
-        private void num7_Click(object sender, EventArgs e)
-        {
-            box.Text += "7";
-        }
-
-        private void num8_Click(object sender, EventArgs e)
-        {
-            box.Text += "8";
-        }
-
-        private void num9_Click(object sender, EventArgs e)
-        {
-            box.Text += "9";
+            else
+            {
+                box.Text = "0";
+            }
         }
 
         private void num0_Click(object sender, EventArgs e)
         {
-            box.Text += "0";
+            if (box.Text != "0")
+                box.Text += "0";
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "1";
+            else
+                box.Text += "1";
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "2";
+            else
+                box.Text += "2";
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "3";
+            else
+                box.Text += "3";
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "4";
+            else
+                box.Text += "4";
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "5";
+            else
+                box.Text += "5";
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "6";
+            else
+                box.Text += "6";
+        }
+
+        private void num7_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "7";
+            else
+                box.Text += "7";
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "8";
+            else
+                box.Text += "8";
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            if (box.Text == "0")
+                box.Text = "9";
+            else
+                box.Text += "9";
         }
 
         private void dota_Click(object sender, EventArgs e)
@@ -85,8 +120,12 @@ namespace ComplexCalculator
 
         private void diff_Click(object sender, EventArgs e)
         {
-            box.Text += "-";
+            if (box.Text == "0")
+                box.Text = "-";
+            else
+                box.Text += "-";
         }
+
 
         private void multi_Click(object sender, EventArgs e)
         {
@@ -97,19 +136,57 @@ namespace ComplexCalculator
         {
             box.Text += "/";
         }
-
+        private void mood_Click(object sender, EventArgs e)
+        {
+            box.Text+= "%";
+        }
         private void sqrt_Click(object sender, EventArgs e)
         {
-            double result = double.Parse(box.Text);
-            result = (double)System.Math.Sqrt(result);
-            box.Text = result.ToString();
+            try
+            {
+                double result = double.Parse(box.Text);
+                if (result < 0)
+                {
+                    MessageBox.Show("Mənfi ədədin kvadrat kökü alınmır!", "Error");
+                    return;
+                }
+                result = Math.Sqrt(result);
+                box.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Düzgün ədəd daxil edin.", "Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Error");
+            }
         }
 
         private void equals_Click(object sender, EventArgs e)
         {
-            string expression = box.Text.Replace("x", "*").Replace(",", ".");
-            var result = new System.Data.DataTable().Compute(expression, null);
-            box.Text = result.ToString();
+            try
+            {
+                string expression = box.Text.Replace("x", "*").Replace(",", ".");
+                if (expression.Contains("/0"))
+                {
+                    MessageBox.Show("0-a bölmək mümkün deyil!", "Error");
+                    return;
+                }
+
+                var result = new System.Data.DataTable().Compute(expression, null);
+                box.Text = result.ToString();
+            }
+            catch (SyntaxErrorException)
+            {
+                MessageBox.Show("Daxil edilmiş ifadə düzgün deyil.", "Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Xəta baş verdi: " + ex.Message, "Error");
+            }
         }
-}
+
+        
     }
+}
